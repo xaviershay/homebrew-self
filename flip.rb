51426f256be21d5d8443006be94e68868a5c44eb
Xavier Shay <xavier@squareup.com>
2017-05-22 08:33:37 -0700

    Replace deprecated sha1, remove really old bottles rather than update.

--- flip.rb
+++ flip.rb
@@ -4,7 +4,7 @@ class Flip < Formula
   homepage "https://github.com/anthonybishopric/flip"
   url "https://github.com/anthonybishopric/flip/archive/master.tar.gz"
   head "https://github.com/anthonybishopric/flip.git"
-  sha1 "42f6ecf390864daeff1e960972a5ca647ec832f3"
+  sha256 "6130e07e50e764d655e5062d47005244be262f4fd3ecf2ece0e2578449cecef4"
   version '0.1'
 
   depends_on 'go' => :build
@@ -14,11 +14,5 @@ class Flip < Formula
     system 'go', 'get', '-d'
     system 'go', 'build', 'flip.go'
     bin.install 'flip'
-  end
-
-  bottle do
-    root_url 'https://raw.githubusercontent.com/xaviershay/homebrew-bottles/master'
-    cellar :any
-    sha1 "5b41b8a27cb3a9d3a575a1390b622ba711a730df" => :mavericks
   end
 end
--- justrun.rb
+++ justrun.rb
@@ -3,7 +3,7 @@ require "formula"
 class Justrun < Formula
   homepage "https://github.com/jmhodges/justrun"
   url "https://github.com/jmhodges/justrun/archive/v1.0.0.tar.gz"
-  sha1 "6957f797e9e5f79eaca1bac8460c37e6160f988b"
+  sha256 "f7aaedfbd1812cddac3ba0aa8646eae4a0256dc083d30bb64459462a84ae946d"
 
   depends_on 'go' => :build
 
@@ -17,6 +17,6 @@ class Justrun < Formula
   bottle do
     root_url 'https://raw.githubusercontent.com/xaviershay/homebrew-bottles/master'
     cellar :any
-    sha1 "d20bffcf5f3a2710e513a6a2b0cf1543e3ef4153" => :mavericks
+    sha256 "d20bffcf5f3a2710e513a6a2b0cf1543e3ef4153" => :mavericks
   end
 end
